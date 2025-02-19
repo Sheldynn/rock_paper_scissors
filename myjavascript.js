4894f26d8b0c4569ea2dc6eb79c7ae79e01e9e54
Steven Denmark <stevendenmark27@gmail.com>
2025-02-19 15:01:09 -0500

    Deleted unnecessary comments.

--- myjavascript.js
+++ myjavascript.js
@@ -1,41 +1,22 @@
 /* PSEUDOCODE SECTION */
-//It turns out that I overcomplicated things a bit here. There's a much simpler way to code this as I've recently learned!
+
 /*
 My goal is to create a simple program that emulates the Rock-Paper-Scissors game
 via the Console. It will:
     1.) *Prompt the User to input (button select method) either Rock, Paper, or Scissors.*
     2.) *Randomly generate either a Rock, Paper, or Scissors value in response to User.*
     3.) *Then, it will evaluate the pair of values and determine the winner.*
-    4.) *In a later edition, it will tabulate a score until winning score is met by
+    4.) *It will then tabulate a score until winning score is met by
         either the User or Console.*
     5.) *It will then display various cheezy responses for winning, losing, or tying.*
 */
-
-
-
-// The array has 3 items indexed at 0, 1, & 2, corresponding to: rock, paper, scissors, respectively. 
 
 //Later on, these variables will be used as input for the GUI witin HTML.
  const playerDisplay = document.getElementById("playerDisplay");
  const computerDisplay = document.getElementById("computerDisplay");
  const resultDisplay = document.getElementById("resultDisplay");
 
- /*here, I'll calculate the computer choice within the variable assignment
- using two call methods to two standard functions within the Math library: floor & random
- but I will call it within the function so that it resets by itself each iteration 
- */
- //This function is to address my ocd for capitalizing the first letter of Rock, Paper, & Scissors, regardless of how the user inputs them. 
- //Eventually, user input will be based on button clicking anyway, but still. This is satisfying.
- function capitalizeFirstLetter(word) {
-    return word
-           .charAt(0)
-           .toUpperCase()
-           + word.substr(1)
-           .toLowerCase();
-        }
-
-
-// Get computer choice, randomly
+// Get computer choice randomly using an array.
 const choices = ["rock", "paper", "scissors"]; //using and array makes sense here for the randomNumber function.
 
 function getComputerChoice(choice) {
@@ -132,8 +113,6 @@ let computerScore=0;
         }
     }
 
-    //console.log(result + " | Player choice: " + capitalizeFirstLetter(playerSelection) + " | " + "Computer choice: " + capitalizeFirstLetter(computerSelection) + " | " + " Player Score: " + playerScore + " | " + "Computer Score: " + computerScore);
-    //console.log(typeof(playerSelection) + " " + typeof(computerSelection));
 }
 
 // Checking that the external javascript file is successfully linked. 
@@ -141,8 +120,13 @@ console.log("Hello World!");
 console.log("You have to specify the relative path file for an externally linked js file, such as the ./ abbreviation, where the dot represents current directory!");
 //Ahh, you live and you learn. 
 
-//Step 4: keeping score
+ //This function is to address my ocd for capitalizing the first letter of Rock, Paper, & Scissors, regardless of how the user inputs them. 
+ //Eventually, user input will be based on button clicking anyway, but still. This is satisfying.
+ function capitalizeFirstLetter(word) {
+    return word
+           .charAt(0)
+           .toUpperCase()
+           + word.substr(1)
+           .toLowerCase();
+        }
 
-
-
-//Step 5: write logic for playing a single round of the game
